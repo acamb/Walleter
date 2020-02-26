9be91a6931644edd13f3c86c06818381c05c5c4a
andrea cambieri <andrea.cambieri@spaceinformatica.com>
2020-02-26 08:59:54 +0100

    cors for local test

--- src/main/groovy/acambieri/walleter/config/WebConfig.java
+++ src/main/groovy/acambieri/walleter/config/WebConfig.java
@@ -1,6 +1,8 @@
 package acambieri.walleter.config;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -8,12 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  *         Date: 11/07/2017
  */
 @org.springframework.context.annotation.Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/static/**").addResourceLocations("/");
     }
-    
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+    }
 }
